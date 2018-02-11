285855b03c489d3c957ba7e83d1b196e848acdc2
Derek Lin <dereklin@hotmail.com>
2018-02-10 17:57:21 -0800

    fix nx generate issues

--- apps/app1/src/app/app.providers.ts
+++ apps/app1/src/app/app.providers.ts
@@ -1,9 +1,9 @@
-import { RouterStateSerializer } from "@ngrx/router-store";
-import { CustomSerializer } from "./store";
-import { AppEffects } from "./+state/app.effects";
-import { Dummy1Service } from "@nrwl-nx-workspace-demo/dummy1-service";
-import * as fromServices from "./services";
-import { MovieResolver } from "@nrwl-nx-workspace-demo";
+import { RouterStateSerializer } from '@ngrx/router-store';
+import { CustomSerializer } from './store';
+import { AppEffects } from './+state/app.effects';
+import { Dummy1Service } from '@nrwl-nx-workspace-demo/dummy1-service';
+import * as fromServices from './services';
+import { MovieResolver } from '@nrwl-nx-workspace-demo';
 
 export const providers = [
   { provide: RouterStateSerializer, useClass: CustomSerializer },
