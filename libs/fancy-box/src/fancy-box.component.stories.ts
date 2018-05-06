a26df0a868d899b0d6edf77a6f37b5bf6f757010
Derek Lin <derek.lin@blackrock.com>
2018-05-06 01:12:55 -0700

    add fancybox story

---
+++ libs/fancy-box/src/fancy-box.component.stories.ts
@@ -0,0 +1,75 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { storiesOf } from '@storybook/angular';
+import { FancyBoxComponent } from './fancy-box.component';
+
+storiesOf('LIBS|FancyBox/FancyBoxComponent', module)
+  .add('Exposure', () => ({
+    moduleMetadata: {
+      imports: [],
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [FancyBoxComponent],
+      providers: []
+    },
+    props: {
+      config: {
+        selectable: true,
+        order: 'ABCD',
+        selected: false,
+        showRemoveMark: false,
+        useSemantic: true
+      }
+    },
+    styles: [
+      `
+      .context {
+        height: 100%;
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      fancy-box {
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: auto 0;
+        grid-template-areas:
+          'order order order order order order'
+          'size size size size tip tip';
+
+        ::ng-deep {
+          .size,
+          .ship,
+          .tip {
+            display: none;
+          }
+          .order {
+            justify-content: center;
+          }
+        }
+      }
+
+      fancy-box::ng-deep .size,
+      fancy-box::ng-deep .ship,
+      fancy-box::ng-deep .tip {
+        display: none !important;
+      }
+
+      fancy-box::ng-deep .order {
+        justify-content: center !important;
+      }
+
+      fancy-box::ng-deep .ui.mini.buttons .button,
+      fancy-box::ng-deep .ui.mini.buttons .or,
+      fancy-box::ng-deep .ui.mini.button {
+        font-size: 0.5rem;
+      }
+
+    `
+    ],
+    template: `
+    <div class="context">
+    <fancy-box [config]="config">
+              </fancy-box>
+    </div>
+              `
+  }));
---
+++ libs/fancy-box/src/fancy-box.component.ts
@@ -0,0 +1,67 @@
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Output
+} from '@angular/core';
+
+// won't be used if useSemantic is set to false
+declare const $: any;
+
+@Component({
+  selector: 'fany-box',
+  templateUrl: './fancy-box.component.html',
+  styleUrls: ['./fancy-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FancyBoxComponent implements AfterViewInit, OnInit {
+  @Input() public config: any;
+
+  @Output() public orderClick = new EventEmitter();
+  @Output() public doubleClick = new EventEmitter();
+  @Output() public removeClick = new EventEmitter();
+
+  @HostBinding('class.selectable') public selectable: boolean;
+  @HostBinding('class.show-remove-mark') public showRemoveMark: boolean;
+
+  public sizeText;
+
+  constructor(private eRef: ElementRef) {}
+
+  public ngOnInit() {
+    this.selectable = this.config.selectable;
+    this.showRemoveMark = !this.config.useSemantic && this.config.showRemoveMark;
+    this.sizeText = 'test';
+  }
+
+  public ngAfterViewInit() {
+    if (this.config.useSemantic) {
+      setTimeout(() => {
+        $(this.eRef.nativeElement).dimmer({
+          on: 'hover',
+          opacity: 0.2,
+          duration: {
+            show: 100,
+            hide: 100
+          }
+        });
+      }, 100);
+    }
+  }
+
+  @HostListener('click')
+  public onClick() {
+    this.orderClick.emit();
+  }
+
+  @HostListener('dblclick')
+  public onDoubleClick() {
+    this.doubleClick.emit();
+  }
+}
