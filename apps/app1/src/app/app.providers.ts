8d439779a344b2eb5e99d0e7b2f29090f927d305
Derek Lin <dereklin@hotmail.com>
2018-07-24 21:56:40 -0700

    use baseHref in angular.json

--- apps/app1/src/app/app.providers.ts
+++ apps/app1/src/app/app.providers.ts
@@ -4,11 +4,13 @@ import { AppEffects } from './+state/app.effects';
 import { Dummy1Service } from '@nrwl-nx-workspace-demo/dummy1-service';
 import * as fromServices from './services';
 import { MovieResolver } from '@nrwl-nx-workspace-demo/movie-resolver';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const providers = [
   { provide: RouterStateSerializer, useClass: CustomSerializer },
   AppEffects,
   Dummy1Service,
   ...fromServices.services,
-  MovieResolver
+  MovieResolver,
+  { provide: APP_BASE_HREF, useValue: '/' }
 ];
