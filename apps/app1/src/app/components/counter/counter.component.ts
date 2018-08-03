60e666cdcae304f5a000d057f97c7f1bc57bd026
Derek Lin <dereklin@hotmail.com>
2018-08-02 22:48:15 -0700

    add glow effect

--- apps/app1/src/app/components/counter/counter.component.ts
+++ apps/app1/src/app/components/counter/counter.component.ts
@@ -3,16 +3,23 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.scss']
 })
 export class CounterComponent implements OnInit {
   public counter = 0;
+  public changed = false;
+
   constructor(private cd: ChangeDetectorRef) {}
 
   public ngOnInit() {
     setInterval(() => {
       this.counter++;
+      this.changed = true;
+      setTimeout(() => {
+        this.changed = false;
+        this.cd.markForCheck();
+      }, 1000);
       this.cd.markForCheck();
-    }, 1000);
+    }, 2000);
   }
 }
