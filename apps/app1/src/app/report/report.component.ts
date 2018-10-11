feef5ef38eb85478c132334fecb049e3df8b1d63
Derek Lin <dereklin@hotmail.com>
2018-10-11 00:07:36 -0700

    test pdf export

--- apps/app1/src/app/report/report.component.ts
+++ apps/app1/src/app/report/report.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
+import * as jspdf from 'jspdf';
 
 @Component({
   selector: 'app-report',
@@ -9,5 +11,22 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 export class ReportComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  public ngOnInit() {}
+
+  public captureScreen() {
+    const data = document.getElementById('contentToConvert');
+    html2canvas(data).then((canvas) => {
+      // Few necessary setting options
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('MYPdf.pdf'); // Generated PDF
+    });
+  }
 }
--- apps/app1/src/app/report/report.module.ts
+++ apps/app1/src/app/report/report.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material';
 
 import { ReportRoutingModule } from './report-routing.module';
 import { ReportComponent } from './report.component';
 
 @NgModule({
-  imports: [CommonModule, ReportRoutingModule],
+  imports: [CommonModule, ReportRoutingModule, MatCardModule],
   declarations: [ReportComponent]
 })
 export class ReportModule {}
