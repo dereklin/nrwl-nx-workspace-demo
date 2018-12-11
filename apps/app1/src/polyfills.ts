8a331a3577f3d5d286c3f49755a7a991dfcbdaad
Derek Lin <dereklin@hotmail.com>
2018-12-10 23:07:39 -0800

    use webcomponent polyfill

--- apps/app1/src/polyfills.ts
+++ apps/app1/src/polyfills.ts
@@ -13,6 +13,13 @@
  *
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
+/* CUSTOM ELEMENTS */
+
+// Used for browsers with partially native support of Custom Elements
+import '@webcomponents/custom-elements/src/native-shim';
+
+// Used for browsers without a native support of Custom Elements
+import '@webcomponents/custom-elements/custom-elements.min';
 
 /***************************************************************************************************
  * BROWSER POLYFILLS
