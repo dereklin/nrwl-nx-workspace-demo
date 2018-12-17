10b7dedac72f47e70d4fc252f04c9404c8cdff1a
Derek Lin <dereklin@hotmail.com>
2018-12-17 14:06:54 -0800

    test n x n matrix rotation

--- libs/algorithms/src/lib/services/rotate-nxn-matrix.service.spec.ts
+++ libs/algorithms/src/lib/services/rotate-nxn-matrix.service.spec.ts
@@ -3,10 +3,33 @@ import { TestBed } from '@angular/core/testing';
 import { RotateNxnMatrixService } from './rotate-nxn-matrix.service';
 
 describe('RotateNxnMatrixService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: RotateNxnMatrixService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(RotateNxnMatrixService);
+  });
 
   it('should be created', () => {
-    const service: RotateNxnMatrixService = TestBed.get(RotateNxnMatrixService);
     expect(service).toBeTruthy();
+  });
+
+  it('should be rotate n x n matrix with extra space', () => {
+    const anArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    const expected = [[7, 4, 1], [8, 5, 2], [9, 6, 3]];
+    expect(service.rotateExtraSpace(anArray)).toEqual(expected);
+  });
+
+  it('should be rotate 3 by 3 matrix in place', () => {
+    const anArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    const expected = [[7, 4, 1], [8, 5, 2], [9, 6, 3]];
+    service.rotateInPlace(anArray);
+    expect(anArray).toEqual(expected);
+  });
+
+  it('should be rotate 4 by 4 matrix in place', () => {
+    const anArray = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+    const expected = [[13, 9, 5, 1], [14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]];
+    service.rotateInPlace(anArray);
+    expect(anArray).toEqual(expected);
   });
 });
--- libs/algorithms/src/lib/services/rotate-nxn-matrix.service.ts
+++ libs/algorithms/src/lib/services/rotate-nxn-matrix.service.ts
@@ -4,6 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RotateNxnMatrixService {
+  constructor() {}
 
-  constructor() { }
+  public rotateExtraSpace(matrix: number[][]) {
+    const b = new Array(matrix.length).fill(0).map(() => new Array(matrix.length).fill(0));
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        b[j][matrix.length - 1 - i] = matrix[i][j];
+      }
+    }
+    return b;
+  }
+
+  public rotateInPlace(matrix: number[][]): any {
+    // tslint:disable
+    debugger;
+    // reverse the rows
+    matrix = matrix.reverse();
+
+    // swap the symmetric elements
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < i; j++) {
+        const temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+  }
 }
