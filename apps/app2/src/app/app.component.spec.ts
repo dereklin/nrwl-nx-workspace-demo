d496b4de17b783ff8cf68179fa7909286fad083c
Derek Lin <linde@linde-a01.vmware.com>
2019-07-01 16:07:04 -0700

    fix tests

--- apps/app2/src/app/app.component.spec.ts
+++ apps/app2/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      declarations: [AppComponent, HelloComponent]
     }).compileComponents();
   }));
 
