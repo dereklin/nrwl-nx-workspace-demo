323d5ac7d8b1d310418f98852691627f27760387
Derek Lin <linde@vmware.com>
2019-09-30 10:40:05 -0700

    using lib-show-random-pic with images

--- apps/app1/src/app/feature1/feature1.module.ts
+++ apps/app1/src/app/feature1/feature1.module.ts
@@ -6,9 +6,17 @@ import { CounterModule } from '../components/counter/counter.module';
 import { GlowingValueModule } from '../components/glowing-value/glowing-value.module';
 import { SparklinesModule } from '../components/sparklines/sparklines.module';
 import { Feature1Component } from './feature1.component';
+import { ShowRandomPicModule } from 'show-random-pic';
 
 @NgModule({
-  imports: [CommonModule, SparklinesModule, CounterModule, GlowingValueModule, ClassToggleOnChangeModule],
+  imports: [
+    CommonModule,
+    SparklinesModule,
+    CounterModule,
+    GlowingValueModule,
+    ClassToggleOnChangeModule,
+    ShowRandomPicModule
+  ],
   declarations: [Feature1Component]
 })
 export class Feature1Module {}
